168529c7ae6bb9c47b6a88208500abda1d014c72
James Kokou GAGLO <freemanpolys@gmail.com>
2021-03-17 12:36:59 +0100

    Install latest images for api and ui. Exporting KAFKA_BROKERS while installing dataplane in the same cluster

--- scripts/control-plane/uninstall.sh
+++ scripts/control-plane/uninstall.sh
@@ -4,40 +4,36 @@
 # Ex: export CNO_VERSION="feature/mysql-operator"
 [[ -z "${CNO_VERSION}" ]] && VERSION='main' || VERSION="${CNO_VERSION}"
 
-# Delete the onboarding super-admin
+# Delete ressources
+## Delete the onboarding super-admin
 kubectl -n cno-system  delete -f https://raw.githubusercontent.com/beopencloud/cno/$VERSION/deploy/control-plane/kafka/cno-super-admin.yaml
-
-# Delete a kafka cluster
+## Delete a kafka cluster
 kubectl -n cno-system delete -f https://raw.githubusercontent.com/beopencloud/cno/$VERSION/deploy/control-plane/kafka/kafka.yaml
-
-# Delete kafka operator
-kubectl -n cno-system delete -f https://raw.githubusercontent.com/beopencloud/cno/$VERSION/deploy/control-plane/operator/kafka-strimzi/crds/kafkaOperator.yaml
-
-
-# Delete keycloak Cluster
+## Delete keycloak Cluster
 kubectl -n cno-system delete -f  https://raw.githubusercontent.com/beopencloud/cno/$VERSION/deploy/control-plane/keycloak/keycloak.yaml
-
-# Delete keycloak Operator
-kubectl -n cno-system delete -f  https://raw.githubusercontent.com/beopencloud/cno/$VERSION/deploy/control-plane/keycloak/keycloak-all.yaml
-
-# Delete CNO operator
-kubectl -n cno-system delete -f https://raw.githubusercontent.com/beopencloud/cno/$VERSION/deploy/control-plane/operator/cnoOperator/cno-operator-all.yaml
-
-# Delete CNO service account, role and binding
+## Delete CNO service account, role and binding
 kubectl -n cno-system delete -f https://raw.githubusercontent.com/beopencloud/cno/$VERSION/deploy/control-plane/operator/templates/cno-rbac.yaml
-
 # Delete CNO API
 kubectl -n cno-system delete -f https://raw.githubusercontent.com/beopencloud/cno/$VERSION/deploy/control-plane/onboarding-api/cno-api.yaml
-
-# Delete Mysql Operator and cluster
+# Delete cluster
 kubectl -n cno-system delete -f https://raw.githubusercontent.com/beopencloud/cno/$VERSION/deploy/control-plane/onboarding-api/cno-api-mysql.yaml
-kubectl -n cno-system delete -f https://raw.githubusercontent.com/beopencloud/cno/$VERSION/deploy/control-plane/operator/mysql-operator/mysql-operator.yaml
-
 # Delete CNO UI
 kubectl -n cno-system delete -f https://raw.githubusercontent.com/beopencloud/cno/$VERSION/deploy/control-plane/onboarding-ui/onboarding-ui.yaml
-
 #Delete remaining resources
 kubectl -n cno-system delete --all  all,ing,secret,cm
+
+
+# Delete crds and operators
+sleep 10
+## Delete kafka operator
+kubectl -n cno-system delete -f https://raw.githubusercontent.com/beopencloud/cno/$VERSION/deploy/control-plane/operator/kafka-strimzi/crds/kafkaOperator.yaml
+## Delete keycloak Operator
+kubectl -n cno-system delete -f  https://raw.githubusercontent.com/beopencloud/cno/$VERSION/deploy/control-plane/keycloak/keycloak-all.yaml
+## Delete CNO operator
+kubectl -n cno-system delete -f https://raw.githubusercontent.com/beopencloud/cno/$VERSION/deploy/control-plane/operator/cnoOperator/cno-operator-all.yaml
+## Delete Mysql Operator
+kubectl -n cno-system delete -f https://raw.githubusercontent.com/beopencloud/cno/$VERSION/deploy/control-plane/operator/mysql-operator/mysql-operator.yaml
+
 
 # Delete cno namespace
 kubectl delete namespace cno-system
