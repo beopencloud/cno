87dc5f75d119a3d423561f9132e9ddd127e14ea6
Mody Sow <mody.sow@beopenit.com>
2021-04-15 18:46:40 +0000

    fixe bug ingress adress status

--- deploy/ingress-controller/nginx/v1.11.1/install.sh
+++ deploy/ingress-controller/nginx/v1.11.1/install.sh
@@ -6,3 +6,4 @@ then
 else
     kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.45.0/deploy/static/provider/cloud/deploy.yaml
 fi
+kubectl -n ingress-nginx patch deployments ingress-nginx-controller --type "json" -p '[{"op":"add","path":"/spec/template/spec/containers/0/args/-","value":"report-ingress-status"},{"op":"add","path":"/spec/template/spec/containers/0/args/-","value":"external-service ingress-nginx-controller"}]'
